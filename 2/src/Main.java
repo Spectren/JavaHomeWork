86f3e117930af96d9c03dc6586861cf3e6add1e5
Spectren <ignatov-e@inbox.ru>
2021-06-16 01:49:09 +0500

    123

--- 2/src/Main.java
+++ 2/src/Main.java
@@ -1,3 +1,41 @@
+import java.util.*;
+
 public class Main {
-    
+    public static<T> Collection<T> first(Collection<T> collection) {
+        return new HashSet<>(collection);
+    }
+
+    public static <Key, Val> Map<Val, Collection<Key>> second(Map<? extends Key, ? extends Val> map) {
+        Map<Val, Collection<Key>> invertMap = new TreeMap<>();
+        Set<? extends Key> keyMap = map.keySet();
+
+        for (Key key : keyMap) {
+            Val value = map.get(key);
+            invertMap.compute(value, (v, k) -> {
+                if (k == null) {
+                    k = new TreeSet<>();
+                }
+                k.add(key);
+                return k;
+            });
+        }
+        return invertMap;
+    }
+
+    public static void third(String[] gamers) {
+        String winnerName = "";
+        int maxScore = 0;
+
+        for (String player : gamers) {
+            String[] playerAndScoreSplit = player.split("\\s+");
+
+            int score = Integer.parseInt(playerAndScoreSplit[1]);
+            if (maxScore < score) {
+                maxScore = score;
+                winnerName = playerAndScoreSplit[0];
+            }
+        }
+
+        System.out.print(winnerName);
+    }
 }
