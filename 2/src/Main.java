d3f1e8e1722c44444b010a915c79079277dd8174
Spectren <ignatov-e@inbox.ru>
2021-06-24 23:18:11 +0500

    Second nw, third task

--- 2/src/Main.java
+++ 2/src/Main.java
@@ -22,20 +22,23 @@ public class Main {
         return invertMap;
     }
 
-    public static void third(String[] gamers) {
-        String winnerName = "";
-        int maxScore = 0;
-
-        for (String player : gamers) {
-            String[] playerAndScoreSplit = player.split("\\s+");
-
-            int score = Integer.parseInt(playerAndScoreSplit[1]);
-            if (maxScore < score) {
-                maxScore = score;
-                winnerName = playerAndScoreSplit[0];
+    public static String third(String[] entries) {
+        HashMap<String, Integer> scores = new HashMap<>();
+        String winner_name = "-";
+        int winner_score = 0;
+        for (String entry : entries) {
+            String[] e = entry.split(" ");
+            String name = e[0];
+            int score = Integer.parseInt(e[1]);
+            int prev_score = scores.getOrDefault(name, 0);
+            int cur_score = prev_score + score;
+            scores.put(name, cur_score);
+            if (cur_score > winner_score) {
+                winner_score = cur_score;
+                winner_name = name;
             }
         }
-
-        System.out.print(winnerName);
+        return winner_name;
     }
+
 }
